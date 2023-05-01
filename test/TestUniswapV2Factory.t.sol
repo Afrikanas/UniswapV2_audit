3ae3908feab0bf50cfb0a8983a9346c69a667f3c
Sean Emile <seanemile.se@protonmail.com>
2023-05-01 19:30:21 +0300

    Added Uniswap Factory Interface

---
+++ test/TestUniswapV2Factory.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import "forge-std/Test.sol";
+import {UniswapV2Factory} from "./interfaces/IUniswapV2Factory.sol";
+
+contract TestUniswapV2Factory is Test {
+    address public factory = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
+
+    function setUp() public {}
+}
